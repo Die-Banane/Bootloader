689b8e566aea893d2053ba4be9748b9b027f8fe2
Jonas Grammann <jonasgrammann@gmail.com>
2026-05-14 18:01:22 +0200

    method to enable the A20 gate

---
+++ a20.asm
@@ -0,0 +1,141 @@
+enable_a20:
+    call check_a20
+    cmp ax, 1
+    je .done
+
+    call enable_a20_bios
+    call check_a20
+    cmp ax, 1
+    je .done
+
+    call enable_a20_kbc
+    call check_a20
+    cmp ax, 1
+    je .done
+
+    call enable_a20_fast
+    call check_a20
+    cmp ax, 1
+    je .done
+    
+    jmp .fail
+
+.done:
+    mov si, a20_done_msg
+    ret
+
+.fail:
+    mov si, a20_failed_msg
+    ret
+
+;check if the a20 line is enabled, store result in ax. 1 - enabled, 0 - disabled
+check_a20:
+    pushf
+    push es
+    push ds
+    push di
+    push si
+    
+    cli
+
+    xor ax, ax
+    mov ds, ax			;ds:si = 0x0000:0x0500 = 0x500
+    mov si, 0x500
+
+    not ax
+    mov es, ax			;es:di = 0xffff:0x0510 = 0x100500
+    mov di, 0x510
+;save previous values
+    push [ds:si]
+    push [es:di]
+
+    mov ah, 1
+    mov byte [ds:si], 1
+    mov byte [es:di], 0
+    mov al, [ds:si]
+    cmp al, [es:di]
+    jne .done
+    dec ah
+
+.done:
+    sti
+    shr ax, 8
+    pop [es:di]
+    pop [ds:si]
+    pop si
+    pop di
+    pop ds
+    pop es
+    popf
+    ret
+
+;try enableing a20 with the BIOS
+enable_a20_bios:
+    mov ax, 0x2401
+    int 0x15
+    ret
+
+enable_a20_kbc:
+    cli
+    push ax
+    pushf
+
+    call .wait_in_buf
+    mov al, 0xAD
+    out 0x64, al	;disable keyboard
+
+    call .wait_in_buf
+    mov al, 0xD0
+    out 0x64, al	;write output port to status register
+
+    call .wait_out_buf
+    in al, 0x60
+    push ax		;read the output port and save it
+
+    call .wait_in_buf
+    mov al, 0xD1
+    out 0x64, al	;write next byte in the data port to the output port
+
+    call .wait_in_buf
+    pop ax
+    or al, 2
+    out 0x60, al	;set bit 1 (enable A20) in the output port
+    jmp .done
+
+;input buffer must be clear before sending commands to the controller
+.wait_in_buf:
+    in al, 0x64		
+    test al, 2		;mask of bit 1, wich is set if the input buffer is occupied
+    jnz .wait_out_buf
+    ret
+
+;output buffer must be clear before reading data from port 0x60
+.wait_out_buf:
+    in al, 0x64
+    test al, 1		;mask of bit 0, wich is set if the output buffer is occupied
+    jnz .wait_in_buf
+    ret
+
+.done:
+    call .wait_out_buf
+    mov al, 0xAE
+    out 0x64, al
+
+    call .wait_out_buf
+
+    popf
+    pop ax
+    sti
+    ret
+
+
+enable_a20_fast:
+    push ax
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    pop ax
+    ret
+
+    a20_failed_msg: db "failed to activate A20 gate", 0x0D, 0x0A, 0
+    a20_done_msg: db "activated A20 gate", 0x0D, 0x0A, 0
--- main.asm
+++ main.asm
@@ -30,7 +30,7 @@ clr_screen:
 
 
 ;prints a null terminated String to the cursor position
-puts:
+print:
     pushf
     push si
     push ax
@@ -54,50 +54,6 @@ puts:
     popf
     ret
 
-;check if the a20 line is enabled, store result in ax. 1 - enabled, 0 - disabled
-check_a20:
-    pushf
-    push es
-    push ds
-    push di
-    push si
-    
-    cli
-
-    xor ax, ax
-    mov ds, ax			;ds:si = 0x0000:0x0500 = 0x500
-    mov si, 0x500
-
-    not ax
-    mov es, ax			;es:di = 0xffff:0x0510 = 0x100500
-    mov di, 0x510
-;save previous values
-    push [ds:si]
-    push [es:di]
-
-    mov ah, 1
-    mov byte [ds:si], 1
-    mov byte [es:di], 0
-    mov al, [ds:si]
-    cmp al, [es:di]
-    jne .exit
-    dec ah
-
-.exit:
-
-    sti
-
-    shr ax, 8
-    pop [es:di]
-    pop [ds:si]
-    pop si
-    pop di
-    pop ds
-    pop es
-    popf
-    ret
-
-
 main:
 ;initialisation
     xor ax, ax
@@ -111,32 +67,18 @@ main:
 
 ;put msg string to the screen
     mov si, a20_msg
-    call puts
+    call print
 
-    call check_a20
-    test ax, ax
-    jnz .a20_done
+    call enable_a20
+    call print
 
-    mov ax, 0x2401
-    int 0x15
-    jc .a20_fail
-
-.a20_done:
-    mov si, done_msg
-    call puts
-    jmp .halt
-
-.a20_fail:
-    mov si, fail_msg
-    call puts
-
-.halt:
+.end:
     hlt
-    jmp .halt
+    jmp .end
+
+%include "a20.asm"
 
 a20_msg: db "activating A20-Line...", 0x0d, 0x0a, 0
-done_msg: db "done", 0x0d, 0x0a, 0
-fail_msg: db "failed", 0xd, 0x0a, 0
 
 times 510 - ($ - $$) db 0	;pad the file with 0
 db 0x55				;place the boot signature at byte 510 and 511
